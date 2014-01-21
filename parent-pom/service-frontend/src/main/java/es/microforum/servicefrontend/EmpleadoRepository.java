1073f511811a4c1dfacbe68312a96e72ef3983a5
amontero2000 <amonter2000@hotmail.com>
2014-01-21 13:34:54 +0100

    cambios

--- parent-pom/service-frontend/src/main/java/es/microforum/servicefrontend/EmpleadoRepository.java
+++
@@ -1,7 +0,0 @@
-package es.microforum.servicefrontend;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpleadoRepository extends CrudRepository<Empleado, String> {
-
-}
--- parent-pom/service-frontend/src/main/java/es/microforum/servicefrontend/EmpresaRepository.java
+++
@@ -1,8 +0,0 @@
-package es.microforum.servicefrontend;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface EmpresaRepository extends CrudRepository<Empresa, String>{
-
-}
